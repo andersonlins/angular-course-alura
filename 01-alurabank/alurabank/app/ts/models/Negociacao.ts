6821f49ed08893c3a046d3a50de65ea54b13dfe4
Anderson <anderson.lins101@gmail.com>
2020-08-14 16:24:21 -0400

    init project

---
+++ 01-alurabank/alurabank/app/ts/models/Negociacao.ts
@@ -0,0 +1,29 @@
+class Negociacao {
+
+    _data;
+    _quantidade;
+    _valor;
+
+    constructor(data, quantidade, valor) {
+        this._data = data;
+        this._quantidade = quantidade
+        this._valor = valor;
+    }
+
+    getData() {
+        return this._data;
+    }
+
+    getQtd() {
+        return this._quantidade;
+    }
+
+    getValor() {
+        return this._valor;
+    }
+
+    getVolume() {
+        return this._quantidade * this._valor;
+    }
+
+}
